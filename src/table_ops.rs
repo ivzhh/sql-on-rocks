25ba33dd10c4b18189c8ac5a1c36f23d4f28a465
ivz hh <454153+ivzhh@users.noreply.github.com>
2019-03-07 12:11:40 -0700

    Use straightforward design for 1st version

--- src/table_ops.rs
+++ src/table_ops.rs
@@ -1,113 +1,87 @@
 use rmps;
 use rocksdb::{Error, DB};
-use std::cmp::Ordering;
-use std::{string, vec};
+use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum ColumnDataType {
-    Int,
-    Text,
+    Int(String),
+    Text(String),
+}
+
+impl ColumnDataType {
+    pub fn get_name(&self) -> &String {
+        match self {
+            ColumnDataType::Int(name) => name,
+            ColumnDataType::Text(name) => name,
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub enum Nullity {
-    PrimaryKey,
-    Nullable,
-    NonNullable,
+pub enum ColumnType {
+    PrimaryKey(ColumnDataType),
+    Nullable(ColumnDataType),
+    NonNullable(ColumnDataType),
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct ColumnDefinition {
-    name: String,
-    data_type: ColumnDataType,
-    nullity: Nullity,
-}
-
-impl ColumnDefinition {
-    pub fn cmp(&self, other: &Self) -> Ordering {
-        match self.nullity {
-            Nullity::PrimaryKey => match other.nullity {
-                Nullity::PrimaryKey => self.name.cmp(&other.name),
-                _ => Ordering::Greater,
-            },
-            _ => match other.nullity {
-                Nullity::PrimaryKey => Ordering::Less,
-                _ => self.name.cmp(&other.name),
-            },
+impl ColumnType {
+    pub fn get_name(&self) -> &String {
+        match self {
+            ColumnType::PrimaryKey(data) => data.get_name(),
+            ColumnType::Nullable(data) => data.get_name(),
+            ColumnType::NonNullable(data) => data.get_name(),
         }
     }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Index {
+pub struct TableBuilder {
     name: String,
-    table_name: String,
-    column_indexes: Vec<i32>,
+    columns: Vec<ColumnType>,
 }
 
-pub enum IndexType {
-    Asc,
-    Desc,
-    Unique
-}
-
-impl Index {
-    pub fn index_key(&self, table: &Table) -> Vec<u8> {
-        rmps::to_vec(&(".index", table.name.as_str(), self.name.as_str())).unwrap()
+impl TableBuilder {
+    pub fn new(name: String) -> Self {
+        TableBuilder {
+            name,
+            columns: Vec::new(),
+        }
     }
-    pub fn index_value(&self, table: Table) -> Vec<u8> {
-        rmps::to_vec(&self).unwrap()
+
+    pub fn build_table(self, db: &DB) -> Table {
+        let mut column_map: HashMap<String, ColumnType> = HashMap::new();
+
+        for col in self.columns.to_owned() {
+            column_map.insert(col.get_name().to_owned(), col);
+        }
+
+        let table = Table {
+            name: self.name,
+            column_map,
+        };
+
+        create_table(db, &table).unwrap();
+
+        table
+    }
+    pub fn set_columns(&mut self, columns: &[ColumnType]) -> &Self {
+        self.columns.extend_from_slice(columns);
+        self
     }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Table {
     name: String,
-    indexes: Vec<Index>,
-    // columns are sorted by name and indexed by offset
-    columns: Vec<ColumnDefinition>,
-    // primary key can be one column or combination of several columns
-    primary_keys: Vec<i32>,
-    // no foreign keys for now
+    column_map: HashMap<String, ColumnType>,
 }
 
 impl Table {
-    pub fn new(name: String) -> Self {
-        Table {
-            name,
-            indexes: Vec::new(),
-            columns: Vec::new(),
-            primary_keys: Vec::new(),
-        }
-    }
-
-    pub fn build_table(&mut self, db: &DB) -> &Self {
-        self.execute(db);
-        self
-    }
-
     pub fn table_key(&self) -> Vec<u8> {
         rmps::to_vec(&(".table", self.name.as_str())).unwrap()
     }
     pub fn table_value(&self) -> Vec<u8> {
         rmps::to_vec(&self).unwrap()
-    }
-
-    pub fn set_columns(&mut self, columns: &[ColumnDefinition]) -> &Self {
-        self.columns.extend_from_slice(&columns);
-        self
-    }
-
-    pub fn set_index(&mut self) -> &Self {
-
-    }
-}
-
-impl Table {
-    fn execute(&mut self, db: &DB) -> () {
-        self.columns.sort_by(|a, b| a.name.cmp(&b.name));
-
-        create_table(db, self).unwrap();
     }
 }
 
@@ -115,17 +89,24 @@ pub fn create_table(db: &DB, table: &Table) -> Result<(), Error> {
     db.put(table.table_key().as_slice(), table.table_value().as_slice())
 }
 
-//pub fn insert_into(db: &DB, table: &Table, values: &[FieldValue]) -> Result<(), Error> {}
-
 #[cfg(test)]
 mod tests {
-    use super::{create_table, Table};
+    use super::{Table, TableBuilder};
     use rmps;
     use rocksdb::DB;
 
+    use std::collections::HashMap;
+
     #[test]
     fn test_schema_key() {
-        assert_eq!(Table::new("User".to_owned()).table_key()[0], 0x92);
+        assert_eq!(
+            Table {
+                name: "User".to_owned(),
+                column_map: HashMap::new()
+            }
+            .table_key()[0],
+            0x92
+        );
     }
 
     #[test]
@@ -139,6 +120,10 @@ mod tests {
     #[test]
     fn test_create_table() {
         let db = DB::open_default("./test_folder/").unwrap();
-        Table::new("User".to_owned()).build_table(&db);
+        let table = TableBuilder::new("User".to_owned()).build_table(&db);
+
+        let reply = db.get(table.table_key().as_slice()).unwrap().unwrap();
+
+        assert_eq!(reply.to_vec(), table.table_value());
     }
 }
