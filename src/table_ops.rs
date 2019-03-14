4ede11fedf1652c8fabbd6effbb1755a961298fd
ivz hh <454153+ivzhh@users.noreply.github.com>
2019-03-14 12:01:30 -0600

    Write key is done.

--- src/table_ops.rs
+++ src/table_ops.rs
@@ -93,6 +93,9 @@ pub struct Table {
     columns: Vec<ColumnType>,
 }
 
+/// table name :: column name :: primary key value
+pub const COLUMN_KEY_NUM_COMPONENTS: u32 = 3;
+
 pub struct InsertStatement<'a> {
     table: &'a Table,
     i: i32,
@@ -161,25 +164,69 @@ impl<'a> InsertStatement<'a> {
     }
 
     pub fn execute(&mut self, db: &DB) -> Result<(), Box<InsertError>> {
-        let mut i = 0;
-        for v in self.columns.iter_mut() {
-            if v.len() == 0 {
+        let mut primary_keys: Vec<usize> = Vec::new();
+
+        {
+            // count primary key number
+            // nullable columns will be filled with msgpack nil;
+            // other columns must be filled by user
+            let mut i = 0;
+            for v in self.columns.iter_mut() {
+                if v.len() == 0 {
+                    match self.table.columns[i] {
+                        ColumnType::Nullable(_) => match rmp::encode::write_nil(v) {
+                            Ok(_) => {}
+                            Err(error_from_rocks) => return Err(Box::new(error_from_rocks)),
+                        },
+                        _ => return Err(Box::new(ConsistencyError {})),
+                    };
+                }
+
                 match self.table.columns[i] {
-                    ColumnType::Nullable(_) => match rmp::encode::write_nil(v) {
-                        Ok(_) => {}
-                        Err(error_from_rocks) => return Err(Box::new(error_from_rocks)),
-                    },
-                    _ => return Err(Box::new(ConsistencyError {})),
+                    ColumnType::PrimaryKey(_) => primary_keys.push(i),
+                    _ => {}
                 };
+
+                i += 1;
             }
-
-            i += 1;
         }
 
-        match db.put(b"key", b"value") {
-            Ok(_) => Ok(()),
-            Err(e) => Err(Box::new(e)),
+        if primary_keys.len() == 0 {
+            return Err(Box::new(ConsistencyError {}));
         }
+
+        let mut pkey_component = Vec::<u8>::new();
+
+        rmp::encode::write_array_len(&mut pkey_component, primary_keys.len() as u32).unwrap();
+
+        for pk in primary_keys.iter() {
+            pkey_component.extend_from_slice(self.columns[*pk].as_slice());
+        }
+
+        {
+            // fill in fields into storage engine
+            let mut i = 0;
+            for v in self.columns.iter_mut() {
+                let column_name = self.table.columns[i].get_name();
+
+                let mut key = Vec::<u8>::new();
+
+                rmp::encode::write_array_len(&mut key, COLUMN_KEY_NUM_COMPONENTS).unwrap();
+
+                rmp::encode::write_str(&mut key, self.table.name.as_str()).unwrap();
+                rmp::encode::write_str(&mut key, column_name.as_str()).unwrap();
+                key.extend_from_slice(pkey_component.as_slice());
+
+                match db.put(key.as_slice(), v.as_slice()) {
+                    Ok(_) => {}
+                    Err(e) => return Err(Box::new(e)),
+                }
+
+                i += 1;
+            }
+        }
+
+        return Ok(());
     }
 }
 
@@ -208,13 +255,25 @@ pub fn create_table(db: &DB, table: &Table) -> Result<(), Error> {
 
 #[cfg(test)]
 mod tests {
-    use super::{Table, TableBuilder};
+    use super::{Table, TableBuilder, COLUMN_KEY_NUM_COMPONENTS};
     use rmps;
     use rocksdb::DB;
+    use std::borrow::Borrow;
     use tempdir::TempDir;
 
     use super::{ColumnDataType, ColumnType};
     use std::collections::HashMap;
+
+    #[test]
+    fn test_msgpack_nested_array() {
+        let output = rmps::to_vec(&(1, 2, (3, 4), 5)).unwrap();
+        // [0]: len 4
+        // [1]: 1
+        // [2]: 2
+        // [3]: len 2
+        assert_eq!(output[0], 0x94);
+        assert_eq!(output[3], 0x92);
+    }
 
     #[test]
     fn test_schema_key() {
@@ -252,7 +311,7 @@ mod tests {
     #[test]
     fn test_create_table_ints() {
         if let Ok(tmp_dir) = TempDir::new("") {
-            let db = DB::open_default(tmp_dir.path().to_str().unwrap()).unwrap();
+            let db: DB = DB::open_default(tmp_dir.path().to_str().unwrap()).unwrap();
             let table = TableBuilder::new("User".to_owned())
                 .set_columns(vec![ColumnType::PrimaryKey(ColumnDataType::Int(
                     "id".to_owned(),
@@ -265,6 +324,24 @@ mod tests {
                 .unwrap()
                 .execute(&db)
                 .unwrap();
+
+            {
+                let mut key_prefix = Vec::<u8>::new();
+                rmp::encode::write_array_len(&mut key_prefix, COLUMN_KEY_NUM_COMPONENTS).unwrap();
+
+                rmp::encode::write_str(&mut key_prefix, "User").unwrap();
+                rmp::encode::write_str(&mut key_prefix, "id").unwrap();
+
+                let mut iter = db.prefix_iterator(key_prefix.as_slice());
+
+                for (key, value) in iter {
+                    let decoded_key: (String, String, (i64,)) =
+                        rmps::decode::from_slice(key.borrow()).unwrap();
+
+                    println!("Saw {:?} {:?}", decoded_key, value);
+                }
+            }
+
             table
                 .insert_into()
                 .set_next(1i64)
